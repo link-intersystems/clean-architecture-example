35ec9b721557d18f3372dcea52a1524da51bbbcd
René Link <rene.link@link-intersystems.com>
2023-02-10 06:49:09 +0100

    Use viewer age instead of customer age

--- film-trading/film-trading-usecases/src/test/java/com/link_intersystems/film/listing/FilmListingInteractorTest.java
+++ film-trading/film-trading-usecases/src/test/java/com/link_intersystems/film/listing/FilmListingInteractorTest.java
@@ -38,7 +38,7 @@ class FilmListingInteractorTest {
         FilmListingInteractor.Deps interactorDeps = mock(FilmListingInteractor.Deps.class);
         when(interactorDeps.getRepository()).thenReturn(repository);
 
-        LocalDateTime fixtedDateTime = LocalDateTime.of(2010, 1, 1, 0, 0, 0);
+        LocalDateTime fixtedDateTime = LocalDateTime.of(2020, 1, 1, 0, 0, 0);
         Clock fixedClock = Clock.fixed(fixtedDateTime.toInstant(ZoneOffset.UTC), ZoneOffset.systemDefault());
         when(interactorDeps.getClock()).thenReturn(fixedClock);
 
@@ -49,10 +49,11 @@ class FilmListingInteractorTest {
     }
 
     @Test
-    void listFilmsForMarySmith() {
+    void listFilmsForA10YearOld() {
         FilmListingRequestModel requestModel = new FilmListingRequestModel();
         requestModel.setLanguage(Locale.ENGLISH);
         requestModel.setLenderId(1);
+        requestModel.setViewerAge(10);
 
         FilmListingResponseModel responseModel = filmListingInteractor.listFilms(requestModel);
 
