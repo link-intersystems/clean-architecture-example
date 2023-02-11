56a2283882b658378e6a9a5df9b7ef1f21098c51
René Link <rene.link@link-intersystems.com>
2023-02-11 17:01:21 +0100

    Refactored maven module structure.

--- car-rental/car-rental-entities/src/main/java/com/link_intersystems/rental/CarRentals.java
+++ car-rental/car-rental-entities/src/main/java/com/link_intersystems/rental/CarRentals.java
@@ -6,7 +6,6 @@ import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CarRentals extends AbstractList<CarRental> {
