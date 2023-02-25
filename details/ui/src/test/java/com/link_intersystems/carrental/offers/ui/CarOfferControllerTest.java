35f1241f1ba489d3da1087b1b4f07834f8c93de2
René Link <rene.link@link-intersystems.com>
2023-02-25 16:08:52 +0100

    Renamed package com.link_intersystems.car to com.link_intersystems.carrental.

--- details/ui/src/test/java/com/link_intersystems/carrental/offers/ui/CarOfferControllerTest.java
+++ details/ui/src/test/java/com/link_intersystems/carrental/offers/ui/CarOfferControllerTest.java
@@ -4,12 +4,14 @@ import com.link_intersystems.carrental.offers.CarOfferOutputModerBuilder;
 import com.link_intersystems.carrental.offers.CarOfferResponseBuilder;
 import com.link_intersystems.carrental.offers.CarOffersRequestModel;
 import com.link_intersystems.carrental.offers.CarOffersResponseModel;
+import com.link_intersystems.swing.action.ActionTrigger;
 import com.link_intersystems.time.LocalDateTimeUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
 import java.math.BigDecimal;
+import java.util.concurrent.Semaphore;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,17 +21,26 @@ class CarOfferControllerTest {
     private CarOfferController carOfferController;
     private CarSearchModel carSearchModel;
     private ListModel<CarOfferModel> carOfferListModel;
+    private Semaphore controllerDone;
 
     @BeforeEach
     void setUp() {
+        controllerDone = new Semaphore(0);
+
         carOffersUseCase = new CarOffersUseCaseMock();
-        carOfferController = new CarOfferController(carOffersUseCase);
+        carOfferController = new CarOfferController(carOffersUseCase) {
+            @Override
+            protected void done(CarOffersResponseModel responseModel) {
+                super.done(responseModel);
+                controllerDone.release();
+            }
+        };
         carSearchModel = carOfferController.getCarSearchModel();
         carOfferListModel = carOfferController.getCarOfferListModel();
     }
 
     @Test
-    void searchCars() {
+    void searchCars() throws InterruptedException {
         CarOffersRequestModel requestModel = new CarOffersRequestModel();
         requestModel.setVehicleType("MICRO");
         requestModel.setPickUpDateTime(LocalDateTimeUtils.dateTime("2023-01-15", "08:00:00"));
@@ -60,7 +71,8 @@ class CarOfferControllerTest {
         carSearchModel.getPickupDate().setValue("2023-01-15T08:00:00");
         carSearchModel.getReturnDate().setValue("2023-01-17T08:00:00");
 
-        carOfferController.searchCars();
+        new ActionTrigger(this).performAction(carOfferController);
+        controllerDone.acquire();
 
         assertTrue(carOffersUseCase.isInvoked());
 
