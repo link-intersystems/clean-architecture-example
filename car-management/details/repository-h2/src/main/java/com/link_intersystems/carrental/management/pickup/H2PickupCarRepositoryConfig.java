bf2f117ad3a0a7ee2e2963740ea440b1a8c81b46
René Link <rene.link@link-intersystems.com>
2023-03-09 06:39:17 +0100

    Placed bean.configs inside modules.

--- car-management/details/repository-h2/src/main/java/com/link_intersystems/carrental/management/pickup/H2PickupCarRepositoryConfig.java
+++ car-management/details/repository-h2/src/main/java/com/link_intersystems/carrental/management/pickup/H2PickupCarRepositoryConfig.java
@@ -3,7 +3,7 @@ package com.link_intersystems.carrental.management.pickup;
 import com.link_intersystems.app.context.BeanSelector;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public class PickupRepositoryConfig {
+public class H2PickupCarRepositoryConfig {
 
     public PickupCarRepository getPickupCarRepository(BeanSelector<JdbcTemplate> jdbcTemplateBeanSelector) {
         return new H2PickupCarRepository(jdbcTemplateBeanSelector.select("getManagementJdbcTemplate"));
