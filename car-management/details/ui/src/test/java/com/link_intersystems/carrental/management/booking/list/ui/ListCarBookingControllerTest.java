16f55746b4efd1f060d07b2f51f58f97dc42a0cd
René Link <rene.link@link-intersystems.com>
2023-04-02 13:44:50 +0200

    Added ListCarBookingController test.

---
+++ car-management/details/ui/src/test/java/com/link_intersystems/carrental/management/booking/list/ui/ListCarBookingControllerTest.java
@@ -0,0 +1,51 @@
+package com.link_intersystems.carrental.management.booking.list.ui;
+
+import com.link_intersystems.carrental.management.booking.list.CarBookingResponseModelMock;
+import com.link_intersystems.carrental.management.booking.list.CustomerResponseModelMock;
+import com.link_intersystems.carrental.management.booking.list.ListBookingsUseCaseMock;
+import com.link_intersystems.swing.action.ActionTrigger;
+import com.link_intersystems.swing.action.DirectTaskExecutor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListCarBookingControllerTest {
+
+    private ListBookingsUseCaseMock listBookingsUseCase;
+    private ListCarBookingController controller;
+    private ActionTrigger actionTrigger;
+
+    @BeforeEach
+    void setUp() {
+        listBookingsUseCase = new ListBookingsUseCaseMock();
+        controller = new ListCarBookingController(listBookingsUseCase);
+        controller.setTaskExecutor(new DirectTaskExecutor());
+        actionTrigger = new ActionTrigger(this);
+    }
+
+    @Test
+    void performAction() {
+        CarBookingResponseModelMock responseModel = new CarBookingResponseModelMock();
+        responseModel.setBookingNumber(123);
+        responseModel.setVIN("YV4952CYXE1702329");
+        CustomerResponseModelMock customer = new CustomerResponseModelMock();
+        customer.setFirstname("René");
+        customer.setLastname("Link");
+        responseModel.setCustomer(customer);
+        listBookingsUseCase.addResponseModel(responseModel);
+
+        actionTrigger.performAction(controller);
+
+        ListModel<ListCarBookingModel> carBookingListModel = controller.getCarBookingListModel();
+        assertEquals(1, carBookingListModel.getSize());
+        ListCarBookingModel model = carBookingListModel.getElementAt(0);
+        assertEquals("123", model.getBookingNumber());
+        assertEquals("YV4952CYXE1702329", model.getVin());
+        CustomerModel customerModel = model.getCustomerModel();
+        assertEquals("René", customerModel.getFirstname());
+        assertEquals("Link", customerModel.getLastname());
+    }
+}
---
+++ car-management/use-cases/src/test/java/com/link_intersystems/carrental/management/booking/list/ListBookingsUseCaseMock.java
@@ -0,0 +1,18 @@
+package com.link_intersystems.carrental.management.booking.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListBookingsUseCaseMock implements ListBookingsUseCase {
+
+    private List<CarBookingResponseModel> responseModels = new ArrayList<>();
+
+    @Override
+    public List<CarBookingResponseModel> listBookings() {
+        return responseModels;
+    }
+
+    public void addResponseModel(CarBookingResponseModel responseModel) {
+        this.responseModels.add(responseModel);
+    }
+}
