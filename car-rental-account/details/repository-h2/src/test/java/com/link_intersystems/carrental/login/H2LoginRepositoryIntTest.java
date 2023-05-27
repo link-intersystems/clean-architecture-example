50941dfe581676c3be7a6197b879af684071f73a
René Link <rene.link@link-intersystems.com>
2023-05-27 08:05:28 +0200

    Extracted account stuff to own component.

--- car-rental-account/details/repository-h2/src/test/java/com/link_intersystems/carrental/login/H2LoginRepositoryIntTest.java
+++ car-rental-account/details/repository-h2/src/test/java/com/link_intersystems/carrental/login/H2LoginRepositoryIntTest.java
@@ -1,8 +1,5 @@
 package com.link_intersystems.carrental.login;
 
-import com.link_intersystems.carrental.CarRentalDBExtension;
-import com.link_intersystems.carrental.customer.Customer;
-import com.link_intersystems.carrental.customer.CustomerId;
 import com.link_intersystems.jdbc.JdbcTemplate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,7 +8,7 @@ import java.sql.Connection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@CarRentalDBExtension
+@CarRentalAccountDBExtension
 class H2LoginRepositoryIntTest {
 
     private H2LoginRepository loginRepository;
@@ -23,17 +20,14 @@ class H2LoginRepositoryIntTest {
 
     @Test
     void isLoginExistent() {
-        Customer customer = loginRepository.findUser(new Login("rene.link", new SecurePassword("rene".toCharArray())));
+        User customer = loginRepository.findUser(new Login("rene.link", new SecurePassword("rene".toCharArray())));
 
         assertNotNull(customer);
-        assertEquals(new CustomerId(6), customer.getId());
-        assertEquals("René", customer.getFirstname());
-        assertEquals("Link", customer.getLastname());
     }
 
     @Test
     void isLogingNotExistent() {
-        Customer customer = loginRepository.findUser(new Login("rene.link", new SecurePassword("link".toCharArray())));
+        User customer = loginRepository.findUser(new Login("rene.link", new SecurePassword("link".toCharArray())));
 
         assertNull(customer);
     }
--- car-rental-account/details/ui/src/test/java/com/link_intersystems/carrental/login/ui/LoginActionTest.java
+++ car-rental-account/details/ui/src/test/java/com/link_intersystems/carrental/login/ui/LoginActionTest.java
@@ -1,6 +1,5 @@
 package com.link_intersystems.carrental.login.ui;
 
-import com.link_intersystems.carrental.application.ui.CustomerModel;
 import com.link_intersystems.carrental.login.LoginResponseModel;
 import com.link_intersystems.swing.action.ActionTrigger;
 import com.link_intersystems.swing.action.DirectTaskExecutor;
@@ -25,7 +24,7 @@ class LoginActionTest {
     }
 
     @Test
-    void login(){
+    void login() {
         LoginResponseModel loginResponseModel = new LoginResponseModel();
         loginResponseModel.setCustomerId(6);
         loginResponseModel.setCustomerFirstname("René");
@@ -43,12 +42,7 @@ class LoginActionTest {
         actionTrigger.performAction(loginAction);
 
         assertEquals("", loginModel.getUsername(), "username cleared");
-        assertArrayEquals(new char[]{'\0','\0','\0','\0'}, loginPassword, "login password cleared");
+        assertArrayEquals(new char[]{'\0', '\0', '\0', '\0'}, loginPassword, "login password cleared");
         assertArrayEquals(new char[0], loginModel.getPassword(), "cleared model contains empty password");
-
-        CustomerModel customerModel = loginAction.getCustomerModel();
-        assertEquals(6, customerModel.getId());
-        assertEquals("René", customerModel.getFirstname());
-        assertEquals("Link", customerModel.getLastname());
     }
 }
